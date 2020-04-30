ea8b177594d2644398bd29f07996e9542465cfc9
kaori kohama <kkaori131415@gmail.com>
2020-04-30 14:27:08 +0200

    fixed projects part to responsive

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../.npm/_npx/1786/lib/node_modules/parcel/src/builtins/css-loader.js"}],"../../../.npm/_npx/1786/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/Users/kohamakaori/code/kaori-kk/portfolio/images/background.png":[["background.0063873b.png","images/background.png"],"images/background.png"],"_css_loader":"../../../.npm/_npx/1786/lib/node_modules/parcel/src/builtins/css-loader.js"}],"../../../.npm/_npx/1786/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
